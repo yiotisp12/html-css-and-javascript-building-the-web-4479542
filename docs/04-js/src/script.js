691c03f71ff1c8defdb9eba464fc06cec1b6f1ea
Panos Yiotis <98241591+yiotisp12@users.noreply.github.com>
2025-07-21 03:30:44 +0000

    Finished: Chapter 4: JS Dev Part 7: Putting it all together

--- docs/04-js/src/script.js
+++ docs/04-js/src/script.js
@@ -32,6 +32,18 @@ function dragElement(terrariumElement) {
 
 dragElement(document.getElementById('plant1'));
 dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
+dragElement(document.getElementById('plant7'));
+dragElement(document.getElementById('plant8'));
+dragElement(document.getElementById('plant9'));
+dragElement(document.getElementById('plant10'));
+dragElement(document.getElementById('plant11'));
+dragElement(document.getElementById('plant12'));
+dragElement(document.getElementById('plant13'));
+dragElement(document.getElementById('plant14'));
 
 document.querySelectorAll(".plant").forEach((plant) => {
   plant.onpointerdown = function(){
